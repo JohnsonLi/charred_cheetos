9382a30088824e0a46e41d90ebec720782c84d7c
Joyce Liao <jycqliao@gmail.com>
2019-01-09 14:05:27 -0500

    work

--- app.py
+++ app.py
@@ -11,10 +11,53 @@ app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
 #---------- Main Page ----------
+@app.route("/")
+def home():
+    return render_template("home.html")
 
 #---------- Login/Register----------
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/auth")
+def authenticate():
+    if db.auth_user(request.args["user"], request.args["password"]):
+        session["logged_in"] = request.args["user"]
+        return redirect(url_for("home"))
+    else:
+        flash("username or password is incorrect")
+        return redirect(url_for("login"))
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/adduser")
+def add_user():
+    if(not request.args["user"].strip() or not request.args["password"] or not request.args["confirm_password"]):
+        flash("Please fill in all fields")
+        return redirect(url_for("register"))
+
+    if(db.check_user(request.args["user"])):
+        flash("User already exists")
+        return redirect(url_for("register"))
+
+    if(request.args["password"] != request.args["confirm_password"]):
+        flash("Passwords don't match")
+        return redirect(url_for("register"))
+
+    db.add_user(request.args["user"], request.args["password"])
+    session["logged_in"] = request.args["user"]
+    return redirect(url_for("home"))
 
 #---------- Logout ----------
+@app.route("/logout")
+def logout():
+    try:
+        session.pop("logged_in")
+    finally:
+        return redirect(url_for("login"))
 
 #---------- Random ----------
 
