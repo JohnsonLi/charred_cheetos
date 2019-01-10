43b3df7ce177116c1de2ec35001c2ed2b879eda6
Joyce Liao <jycqliao@gmail.com>
2019-01-10 14:45:49 -0500

    update lb

--- util/db.py
+++ util/db.py
@@ -1,5 +1,5 @@
 import sqlite3
-DB_FILE="data/wordsearch.db"
+DB_FILE="../data/wordsearch.db"
 
 # ===================== Initiate =====================
 def createTable():
@@ -10,10 +10,7 @@ def createTable():
     command = "CREATE TABLE users (username TEXT, password TEXT)"
     c.execute(command)
 
-    command = "CREATE TABLE leaderboard (username TEXT, puzzleID INTEGER, time INTEGER)"
-    c.execute(command)
-
-    command = "CREATE TABLE puzzle (puzzleID INTEGER, words TEXT)"
+    command = "CREATE TABLE leaderboard (username TEXT, category INTEGER, level INTEGER, time INTEGER)"
     c.execute(command)
 
     db.commit()
@@ -53,4 +50,47 @@ def check_user(username):
     db.close()
     return False
 
+# ===================== Leaderboard =====================
+def update_lb(user, category, level, time):
+    """Update leaderboard with new time."""
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+
+    command = "SELECT time FROM leaderboard WHERE username = '{}' and category = {} and level = {}".format(user, category, level)
+    c.execute(command)
+    current_best = c.fetchall()
+
+    # if user has never played the level
+    if (current_best == []):
+        # print("inserting new value")
+        command = "INSERT INTO leaderboard VALUES(?,?,?,?)"
+        c.execute(command, (user, category, level, time))
+    # the user had played the level
+    else:
+        # print(current_best[0][0])
+        # if new time is faster than current time
+        if (time < current_best[0][0]):
+            # print("new best for user " + user)
+            command = "UPDATE leaderboard SET time={} WHERE username = '{}' and category = {} and level = {}".format(time, user, category, level)
+            c.execute(command)
+
+    db.commit()
+    db.close()
+
+
 # createTable()
+def test():
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    # c.execute("SELECT time FROM leaderboard WHERE level = 3")
+    # result = c.fetchall()
+    # print(result)
+
+    # update_lb("a", 1, 1, 30)
+    # update_lb("b", 1, 2, 30)
+    # update_lb("a", 1, 2, 45)
+    # update_lb("a", 1, 1, 25)
+
+    db.close()
+
+# test()
