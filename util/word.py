bff9b053a5223e1746278b5af4bc39b2de1474f9
Cheryl Qian <cqian@stuy.edu>
2019-01-11 13:40:24 -0500

    wordApi works

---
+++ util/word.py
@@ -0,0 +1,17 @@
+import urllib.request, json, ssl, random
+
+dmUrl = "https://api.datamuse.com/words?topic={0}"
+
+def randomWords(list, num):
+    newList = []
+    i = 0
+    while ( i < num ):
+        r = random.randint(0, len(list))
+        i++
+        newList.append(list[i])
+    return newList
+
+
+def categoryWords(category, num):
+    dmUrl.format(category)
+    
